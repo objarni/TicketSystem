3e55a95c48372c25ebecbbc3200aee173df34636
Olof Bjarnason <olof.bjarnason@gmail.com>
2023-05-31 13:32:26 +0200

    r Auto-format (Rider default style for C#)

--- TicketSystem/TicketSystem/TicketService.cs
+++ TicketSystem/TicketSystem/TicketService.cs
@@ -5,26 +5,19 @@ namespace TicketManagementSystem;
 
 public class TicketService
 {
-    public int CreateTicket(string ticketStringlyType, Priority priority, string assignedTo, string description, DateTime created, bool isPayingCustomer)
+    public int CreateTicket(string ticketStringlyType, Priority priority, string assignedTo, string description,
+        DateTime created, bool isPayingCustomer)
     {
         if (ticketStringlyType == null || description == null || ticketStringlyType == "" || description == "")
-        {
             throw new InvalidTicketException("Title or description were null");
-        }
 
         User user = null;
         using (var ur = new UserRepository())
         {
-            if (assignedTo != null)
-            {
-                user = ur.GetUser(assignedTo);
-            }
+            if (assignedTo != null) user = ur.GetUser(assignedTo);
         }
 
-        if (user == null)
-        {
-            throw new UnknownUserException("User " + assignedTo + " not found");
-        }
+        if (user == null) throw new UnknownUserException("User " + assignedTo + " not found");
 
         var priorityRaised = false;
         if (created < DateTime.UtcNow - TimeSpan.FromHours(1))
@@ -41,16 +34,12 @@ public class TicketService
             }
         }
 
-        if ((ticketStringlyType.Contains("Crash") || ticketStringlyType.Contains("Important") || ticketStringlyType.Contains("Failure")) && !priorityRaised)
+        if ((ticketStringlyType.Contains("Crash") || ticketStringlyType.Contains("Important") ||
+             ticketStringlyType.Contains("Failure")) && !priorityRaised)
         {
             if (priority == Priority.Low)
-            {
                 priority = Priority.Medium;
-            }
-            else if (priority == Priority.Medium)
-            {
-                priority = Priority.High;
-            }
+            else if (priority == Priority.Medium) priority = Priority.High;
         }
 
         if (priority == Priority.High)
@@ -65,16 +54,12 @@ public class TicketService
         {
             accountManager = new UserRepository().GetAccountManager();
             if (priority == Priority.High)
-            {
                 price = 100;
-            }
             else
-            {
                 price = 50;
-            }
         }
 
-        var ticket = new Ticket()
+        var ticket = new Ticket
         {
             Title = ticketStringlyType,
             AssignedUser = user,
@@ -95,23 +80,14 @@ public class TicketService
         User user = null;
         using (var ur = new UserRepository())
         {
-            if (username != null)
-            {
-                user = ur.GetUser(username);
-            }
+            if (username != null) user = ur.GetUser(username);
         }
 
-        if (user == null)
-        {
-            throw new UnknownUserException("User not found");
-        }
+        if (user == null) throw new UnknownUserException("User not found");
 
         var ticket = TicketRepository.GetTicket(id);
 
-        if (ticket == null)
-        {
-            throw new ApplicationException("No ticket found for id " + id);
-        }
+        if (ticket == null) throw new ApplicationException("No ticket found for id " + id);
 
         ticket.AssignedUser = user;
 
