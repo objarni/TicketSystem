8599ecc80edfb38b6662e3e4d4056dafa5a1bfdb
Olof Bjarnason <olof.bjarnason@gmail.com>
2023-05-31 14:30:27 +0200

    t Add approval test for one exception path

--- TicketSystem/TicketSystemRegressionTest/TicketSystemRegressionTest.cs
+++ TicketSystem/TicketSystemRegressionTest/TicketSystemRegressionTest.cs
@@ -5,9 +5,9 @@ using TicketManagementSystem;
 
 namespace TicketSystemRegressionTest;
 
+[UseReporter(typeof(DiffReporter))]
 public class Tests
 {
-    [UseReporter(typeof(DiffReporter))]
     [Test]
     public void CreateTicketTest()
     {
@@ -31,6 +31,40 @@ public class Tests
             user);
         var toVerify = JsonSerializer.Serialize(ticket,
             new JsonSerializerOptions { WriteIndented = true });
+
+        Approvals.Verify(toVerify);
+    }    [Test]
+
+    public void CreateTicketTestWithException()
+    {
+        string title = null;
+        var priority = Priority.High;
+        string assignedTo = null;
+        var description = "description";
+        var created = default(DateTime);
+        var isPayingCustomer = false;
+        var user = new User();
+        user.Username = "username";
+        user.FirstName = "Olof";
+        user.LastName = "Bjarnason";
+        string toVerify = "";
+        try
+        {
+            var ticket = TicketService.CreateTicketInner(
+                title,
+                priority,
+                assignedTo,
+                description,
+                created,
+                isPayingCustomer,
+                user);
+            toVerify = JsonSerializer.Serialize(ticket,
+                new JsonSerializerOptions { WriteIndented = true });
+        }
+        catch (Exception e)
+        {
+            toVerify = e.ToString();
+        }
 
         Approvals.Verify(toVerify);
     }
